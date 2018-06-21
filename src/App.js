f15da86b5fb8ce9fe5fb163c94605ef92a7f1ef3
Kirby Domingo <kirby.domingo@gmail.com>
2018-06-21 00:00:19 -0700

    update to use api

--- src/App.js
+++ src/App.js
@@ -1,34 +1,26 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts';
+import * as ContactsAPI from './utils/ContactsAPI';
 
 class App extends Component {
   state = {
-    contacts: [
-      {
-        "id": "karen",
-        "name": "Karen Isgrigg",
-        "handle": "karen_isgrigg",
-        "avatarURL": "http://localhost:5001/karen.jpg"
-      },
-      {
-        "id": "richard",
-        "name": "Richard Kalehoff",
-        "handle": "richardkalehoff",
-        "avatarURL": "http://localhost:5001/richard.jpg"
-      },
-      {
-        "id": "tyler",
-        "name": "Tyler McGinnis",
-        "handle": "tylermcginnis",
-        "avatarURL": "http://localhost:5001/tyler.jpg"
-      }
-     ]
+    contacts: []
   }
 
   removeContact = (contact) => {
-    this.setState((prevState) => ({
-      contacts: prevState.contacts.filter((contactEntry) => contactEntry !== contact )
-    }));
+    ContactsAPI.remove(contact)
+      .then(ContactsAPI.getAll()
+        .then((data) => 
+          this.setState((prevState) => ({ contacts: [...data]}))
+        )
+      );
+  }
+  
+  componentDidMount = () => {
+    ContactsAPI.getAll()
+      .then((data) => 
+        this.setState((prevState) => ({ contacts: [...data]}))
+      );
   }
 
   render() {
